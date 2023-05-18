c56588f765e543e423c1cb0a28125e52e1e146ea
Almendra Romina R <Almendraromina1@gmail.com>
2023-05-17 23:56:03 -0300

    Se agrego carrusel

--- src/app/components/Welcome.js
+++ src/app/components/Welcome.js
@@ -1,3 +1,30 @@
+import { generarCarousel } from "./Carousel.js";
+const data = {
+    listaImagenesNuevas: [
+      "https://i.postimg.cc/XvG9s4nn/Ilustraci-n-Claudia-Pia-B-18-de-marzo2.jpg",
+      "https://i.postimg.cc/RCcLrSKF/Imagen-de-Whats-App-2023-03-31-a-las-13-02-53.jpg",
+      "https://i.postimg.cc/9XRFz0Jm/LAS-VIDAS-TRANS-IMPORTAN.jpg",
+      "https://i.postimg.cc/fT7s09XN/552.jpg",
+    ],
+  };
 export function Welcome() {
+    const $header = document.createElement('header');
+    const propText = 'e';
+    $header.innerHTML = `<h1>Bienvenid<span class="prop">${propText}</span>s a Disidir</h1>`;
+    const propElements = $header.querySelectorAll('.prop');
+    let index = 0;
 
+    function changePropText() {
+        let texts = ['a', '@', 'x', 'o', 'e', '*'];
+        if (index >= texts.length) {
+            index = 0;
+        }
+        for (let i = 0; i < propElements.length; i++) {
+            propElements[i].textContent = texts[index];
+        }
+        index++;
+    }
+    setInterval(changePropText, 1000);
+    $header.appendChild(generarCarousel(data))
+    return $header;
 };
--- src/app/page/Home.js
+++ src/app/page/Home.js
@@ -2,18 +2,20 @@ import { Navbar } from '../components/Navbar.js';
 import { crearCards } from '../helpers/CrearCards.js';
 import { Alert } from '../components/Alert.js';
 import { firebase } from '../helpers/FirebaseData.js';
+import { Welcome } from '../components/Welcome.js';
 
 export async function Home() {
     const $root = document.getElementById('root');
     $root.innerHTML = '';
-    $root.appendChild(Alert())
+
     $root.appendChild(Navbar());
+    $root.appendChild(Welcome());
     const $section = document.createElement('section');
     $section.classList.add('cards');
     const $main = document.createElement('main');
     $main.appendChild($section);
     $root.appendChild($main);
     const data = await firebase('/Laterraza/talleres/0/');
-    console.log(data)
+    console.log(data);
     crearCards(data);
 }
